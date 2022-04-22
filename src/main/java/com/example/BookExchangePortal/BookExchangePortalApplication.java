83da007a5f15a66c838f9450ead5a0e1d9294ec3
Shivam Agrawal <shivam.agr2704@gmail.com>
2022-04-23 03:10:40 +0530

    Added User package

--- src/main/java/com/example/BookExchangePortal/BookExchangePortalApplication.java
+++ src/main/java/com/example/BookExchangePortal/BookExchangePortalApplication.java
@@ -1,13 +1,25 @@
 package com.example.BookExchangePortal;
 
+import java.util.List;
+
+import com.example.BookExchangePortal.user.User;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BookExchangePortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookExchangePortalApplication.class, args);
+	}
+
+	@GetMapping
+	public List<User> hello(){
+		return List.of(new User("Shivam", "1","xxxx", "shivamagrawal@gmail.com", "9876543210", "address", true));
 	}
 
 }
---
+++ src/main/java/com/example/BookExchangePortal/user/User.java
@@ -0,0 +1,89 @@
+package com.example.BookExchangePortal.user;
+
+public class User {
+    private String username;
+    private String userId;
+    private String password;
+    private String email;
+    private String phone;
+    private String address;
+    private Boolean isAdmin;
+
+    public User(String username, String userId, String password, String email, String phone, String address,
+            Boolean isAdmin) {
+        this.username = username;
+        this.userId = userId;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.isAdmin = isAdmin;
+    }
+
+    public User(String username, String password, String email, String phone, String address, Boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    
+}
