4f5c56f48f5843636b5b71b79ea696639a65cac1
anurodh <anurodhchadha2001@gmail.com>
2022-04-27 18:18:50 +0530

    Made user list page

--- src/main/java/com/example/BookExchangePortal/user/UserController.java
+++ src/main/java/com/example/BookExchangePortal/user/UserController.java
@@ -3,12 +3,17 @@ package com.example.BookExchangePortal.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(path = "api/user")
 public class UserController {
